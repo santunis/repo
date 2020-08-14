742429a24f16f0e554a955f5d6a64c7c5fcdb94c
Santunis Monteiro de Oliveira <santunis.oliveira@fornecedores.sicoob.com.br>
2020-08-14 17:54:32 -0300

    ajustes finais

--- repo/src/pages/main/index.js
+++ repo/src/pages/main/index.js
@@ -3,6 +3,13 @@ import api from "../../services/api";
 import Avatar from '@material-ui/core/Avatar';
 import './styles.css';
 
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Typography from '@material-ui/core/Typography';
+
+
 export default class Main extends Component {
 
     state = {
@@ -17,22 +24,36 @@ export default class Main extends Component {
         const response = await api.get();
         this.setState({ items: response.data.items });
     };
-    
+
     render() {
         const { items } = this.state;
         return (
+
+
             <Fragment>
                 <div className="items-list">
                     {items.map(item => (
-                        <article key={item.id}>
-                            <td>
-                                <Avatar variant="square" alt="Remy Sharp" src={item.owner.avatar_url} />
-                            </td>
-                            <td>
-                                <strong>{item.owner.login}</strong>
-                                <p>{item.name}</p>
-                            </td>
-                        </article>
+                        <List>
+                            <ListItem>
+                                <ListItemAvatar>
+                                    <Avatar variant="square" alt="Remy Sharp" src={item.owner.avatar_url} />
+                                </ListItemAvatar>
+                                <ListItemText
+                                    primary={item.owner.login}
+                                    secondary={
+                                        <React.Fragment>
+                                            <Typography
+                                                component="span"
+                                                variant="body2"
+                                                color="textPrimary"
+                                            >
+                                                {item.name}
+                                            </Typography>
+                                        </React.Fragment>
+                                    }
+                                />
+                            </ListItem>
+                        </List>
                     ))}
                 </div>
             </Fragment>
